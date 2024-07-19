3c1e0346d03c3d54589c98f22327595f7457788c
jam peng <pengzhe.info@gmail.com>
2024-07-19 19:08:20 +0800

    註冊功能完成 (使用Microsoft.AspNetCore.Identity套件中使用者管理功能)

---
+++ createWebApi_DominModels/Controllers/AuthController.cs
@@ -0,0 +1,57 @@
+﻿using createWebApi_DominModels.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace createWebApi_DominModels.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AuthController(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+
+        /// <summary>
+        /// 註冊新用戶
+        /// POST: /api/Auth/Register
+        /// </summary>
+        /// <param name="registerRequestDto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [Route("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterRequestDto registerRequestDto)
+        {
+            var identityUser = new IdentityUser
+            {
+                UserName = registerRequestDto.Username,
+                Email = registerRequestDto.Username
+            };
+
+            var identityResult = await userManager.CreateAsync(identityUser, registerRequestDto.Password);
+
+            if(identityResult.Succeeded)
+            {
+                // 新增用戶的腳色權限
+                if(registerRequestDto.Roles != null && registerRequestDto.Roles.Any())
+                {
+                    identityResult = await userManager.AddToRolesAsync(identityUser, registerRequestDto.Roles);
+                
+                    if(identityResult.Succeeded)
+                    {
+                        return Ok("註冊成功");
+                    }
+                }
+            }
+
+            return BadRequest("帳號或密碼已被使用");
+        }
+
+
+    }
+}
---
+++ createWebApi_DominModels/Models/DTO/RegisterRequestDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace createWebApi_DominModels.Models.DTO
+{
+    public class RegisterRequestDto
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+
+        public required string Username {  get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public required string Password { get; set; }
+
+        public required string[] Roles { get; set; } //建立的使用者權限(Reader 或 Writer)
+    }
+}
